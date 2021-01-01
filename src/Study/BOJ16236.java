04025c1406c5548d1a42d392cb78c94a85706f5f
wonhee <wonhee010@gmail.com>
2021-01-01 18:47:34 +0900

    feat: add BOJ16236

---
+++ src/Study/BOJ16236.java
@@ -0,0 +1,13 @@
+package Study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ16236 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }
+}
