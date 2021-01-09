54c45c9d1487ab41c5bebf33c66d57c8b787bad9
wonhee <wonhee010@gmail.com>
2021-01-09 13:44:17 +0900

    feat: add solution

---
+++ src/Test/TidiThree.java
@@ -0,0 +1,14 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TidiThree {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public static void solution() {
+
+    }
+}
--- src/Test/TidiTwo.java
+++ src/Test/TidiTwo.java
@@ -1,17 +1,11 @@
 package Test;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
-public class CouOne {
+public class TidiTwo {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        bw.flush();
-        bw.close();
     }
 
     public static void solution() {
