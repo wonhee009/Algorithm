f2bd348366f18f8ba814164a427fb33af17c31b6
이원희 <wonhee010@gmail.com>
2025-12-16 23:04:54 +0900

    add TwoSum

--- Algorithm/01. Two Sum/TwoSum.swift
+++ Algorithm/01. Two Sum/TwoSum.swift
@@ -9,43 +9,14 @@
 
 class TwoSum {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        // time: n*n, space: 1
-        /*
-        var startIndex = 0
-        var endIndex = nums.count - 1
-        
-        while startIndex < (nums.count - 1) {
-            while endIndex > startIndex {
-                if nums[startIndex] + nums[endIndex] == target {
-                    return [startIndex, endIndex]
-                }
-                endIndex = endIndex - 1
-            }
-            startIndex = startIndex + 1
-            endIndex = nums.count - 1
-        }
-        return []
-         */
-        
-        // time: 1, space: 1
-        /*
-        var numWithIndex: [Int: Int] = [:]
+        var numWithIndex: [Int: Int] [:]
         for (index, num) in nums.enumerated() {
             if let otherIndex = numWithIndex[num] {
-                return [index, otherIndex]
+                return [otherIndex, index]
             } else {
                 numWithIndex[target - num] = index
             }
         }
-        return []
-         */
-        
-// We use a dictionary so we only loop through the array once.
-// For each number, we compute 'target number - number'
-// If that complement is already in a dictionary, we return its index and the current index.
-// Otherwise, we store the current number with index.
-// This runs in O(n) time and uses O(n) extra space.
-        
         return []
     }
 }
