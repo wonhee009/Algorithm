776b13bbc65e2d7468a03be4a90121e02f1234f4
이원희 <wonhee010@gmail.com>
2025-12-16 22:08:32 +0900

    add ReverseString

--- Algorithm/344. Reverse String/ReverseString.swift
+++ Algorithm/344. Reverse String/ReverseString.swift
@@ -1,11 +1,19 @@
 class ReverseString {
     func reverseString(_ s: inout [Character]) {
+        // var left = 0
+        // var right = s.count - 1
+        // while left < right {
+        //     let temp = s[left]
+        //     s[left ] = s[right]
+        //     s[right] = temp
+        //     left += 1
+        //     right -= 1
+        // }
+
         var left = 0
         var right = s.count - 1
         while left < right {
-            let temp = s[left]
-            s[left ] = s[right]
-            s[right] = temp
+            s.swapAt(left, right)
             left += 1
             right -= 1
         }
