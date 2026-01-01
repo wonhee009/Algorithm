eb81943a760ed5730e847f5ae67867b29c4bc617
이원희 <wonhee010@gmail.com>
2026-01-01 14:24:52 +0900

    add ContainsDuplicate

--- Algorithm/217. Contains Duplicate/ContainsDuplicate.swift
+++ Algorithm/217. Contains Duplicate/ContainsDuplicate.swift
@@ -1,12 +1,21 @@
 class ContainsDuplicate {
     func containsDuplicate(_ nums: [Int]) -> Bool {
-        var dict = [Int: Int]()
+        // var dict = [Int: Int]()
+        // for num in nums {
+        //     if let n = dict[num] {
+        //         return true
+        //     } else {
+        //         dict[num] = 1
+        //     }
+        // }
+        // return false
+
+        var set = Set<Int>()
         for num in nums {
-            if let n = dict[num] {
+            if set.contains(num) {
                 return true
-            } else {
-                dict[num] = 1
             }
+            set.insert(num)
         }
         return false
     }
