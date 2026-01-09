a81fbfd18c68a215bd70bbe75e6d577f0d5985a7
이원희 <wonhee010@gmail.com>
2026-01-09 21:37:12 +0900

    add RansomNote

--- Algorithm/383. Ransom Note/RansomNote.swift
+++ Algorithm/383. Ransom Note/RansomNote.swift
@@ -8,21 +8,18 @@
 
 class RansomNote {
     func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
-        // time: O(n+m), space: O(n)
         var magazineCount: [Character: Int] = [:]
         for m in magazine {
             magazineCount[m, default: 0] += 1
         }
-        
-        for n in ransomNote {
-            if let count = magazineCount[n], count > 0 {
-                magazineCount[n] = count - 1
+
+        for r in ransomNote {
+            if let count = magazineCount[r], count > 0 {
+                magazineCount[r] = count - 1
             } else {
                 return false
             }
         }
         return true
-        
-// The main idea behind my solution is to use a dictionary to count how many times each character appears in the magazine. Then I go through each character in the ransom note and check if it’s available in that dictionary with a sufficient count. If any character is missing or we run out of it, I immediately return false. Otherwise, if we get through the whole note, we return true. It’s a straightforward O(n + m) time solution and it’s pretty efficient.
     }
 }
